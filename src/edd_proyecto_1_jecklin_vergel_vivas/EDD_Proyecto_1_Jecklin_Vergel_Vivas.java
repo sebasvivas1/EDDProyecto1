f46c3eec66a2e2e1fc28fdd154b8f7c60f3f586e
Mzterdox <sebasvivas107@gmail.com>
2020-11-10 13:21:27 -0400

    Probando el repo

--- src/edd_proyecto_1_jecklin_vergel_vivas/EDD_Proyecto_1_Jecklin_Vergel_Vivas.java
+++ src/edd_proyecto_1_jecklin_vergel_vivas/EDD_Proyecto_1_Jecklin_Vergel_Vivas.java
@@ -1,12 +1,15 @@
 
 package edd_proyecto_1_jecklin_vergel_vivas;
 
+import javax.swing.JOptionPane;
+
 
 public class EDD_Proyecto_1_Jecklin_Vergel_Vivas {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Hola Mundo");
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
 
 
     }

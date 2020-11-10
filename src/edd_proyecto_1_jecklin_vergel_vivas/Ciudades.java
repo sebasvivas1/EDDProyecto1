7eb2d3d5e42d7340e81a8fcb3a38f157751751c8
Mzterdox <sebasvivas107@gmail.com>
2020-11-10 13:18:43 -0400

    Actualizando el formato del proyecto

--- src/edd_proyecto_1_jecklin_vergel_vivas/Ciudades.java
+++ src/edd_proyecto_1_jecklin_vergel_vivas/Ciudades.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edd_proyecto_1_jecklin_vergel_vivas;
 
-/**
- *
- * @author sebastian
- */
 public class Ciudades {
-    
+
 }

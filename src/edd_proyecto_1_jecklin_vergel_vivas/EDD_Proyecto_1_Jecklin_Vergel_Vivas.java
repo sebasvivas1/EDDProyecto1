d8ba9056a5815b739c5dc3e64a948d610a4ef6ad
Mzterdox <sebasvivas107@gmail.com>
2020-11-14 01:46:01 -0400

    Limpiando el proyecto

--- src/edd_proyecto_1_jecklin_vergel_vivas/EDD_Proyecto_1_Jecklin_Vergel_Vivas.java
+++ src/edd_proyecto_1_jecklin_vergel_vivas/EDD_Proyecto_1_Jecklin_Vergel_Vivas.java
@@ -6,47 +6,7 @@ import javax.swing.JOptionPane;
 public class EDD_Proyecto_1_Jecklin_Vergel_Vivas {
         
     public static void main(String[] args) {
-        double[][] probando;
-        Funciones f = new Funciones();
-        ColoniaHormigas ch = new ColoniaHormigas();
-        String[] list = f.cargarDatos();
-        ListaCiudades ciudades = f.obtenerCiudades(list);
-        //ciudades.listar();
-        f.invertirArreglo(list);
-        double[][] m = f.obtenerCosto(list, 4);
-//        for(int i = 0; i < m.length; i++){ 
-//            for(int j = 0; j < m[i].length; j++){ 
-//                System.out.print(m[i][j] + " ");    // Imprime elemento 
-//            } 
-//            System.out.println();    // Imprime salto de línea 
-//        }
-        double k[][]= ch.InicializarFeromonas(4);
-        double h[][]= ch.InicializarVisibilidad(m, 4);
         
-        double g[][] = ch.probabilidadPorCamino(4, m, h, 1, 2);
-        
-                for(int i = 0; i < g.length; i++){ 
-            for(int j = 0; j < g[i].length; j++){ 
-                System.out.print(g[i][j] + " ");    // Imprime elemento 
-            } 
-            System.out.println();    // Imprime salto de línea 
-        }
-        
-        ColoniaHormigas probarVisibilidad = new ColoniaHormigas();
-        
-        
-     
-            
-            Recorrido r = new Recorrido();
-            
-            r.insertarAlFinal(1);
-            r.insertarAlFinal(2);
-            r.insertarAlFinal(3);
-            
-            
-            System.out.println(r.getTamanio());
-            System.out.println(r.getpLast().getValor());
-            System.out.println(r.enRecorrido(7));
             
     }
 }
